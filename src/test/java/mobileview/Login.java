43e3e3a9aaa33ff96d4cc7f1b720b396887c3310
prasetyo531 <prasetyo531@gmail.com>
2021-04-20 17:47:16 +0700

    test

--- src/test/java/mobileview/Login.java
+++ src/test/java/mobileview/Login.java
@@ -1,4 +1,60 @@
 package mobileview;
 
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Login {
+
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    @BeforeMethod
+    public void setup () throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "RR8M90CMW9N");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UIAutomator2");
+        capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "Chrome");
+        //appium --allow-insecure chromedriver_autodownload
+        capabilities.setCapability("chromedriverExecutableDir","/Users/fdn-prasetyo/Documents/chromedriver");
+        driver = new AndroidDriver(new URL("http://0.0.0.1:4723/wd/hub"),capabilities);
+        wait = new WebDriverWait(driver, 10);
+
+        driver.get("https://femaledaily.com");
+    }
+
+    @Test
+    public void basicTest () throws InterruptedException {
+        //Click and pass Splash
+        wait.until(ExpectedConditions.visibilityOfElementLocated
+                (By.id("com.isinolsun.app:id/animation_view"))).click();
+
+        //Click I am searching a job
+        wait.until(ExpectedConditions.visibilityOfElementLocated
+                (By.id("com.isinolsun.app:id/bluecollar_type_button"))).click();
+
+
+        //Notification Allow
+        if (driver.findElements(By.id("com.android.packageinstaller:id/permission_allow_button")).size()>0) {
+            driver.findElements(By.id("com.android.packageinstaller:id/permission_allow_button")).get(0).click();
+        }
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated
+                (By.xpath(secondNewJob)));
+    }
+
+    @AfterMethod
+    public void teardown(){
+        driver.quit();
+    }
 }
